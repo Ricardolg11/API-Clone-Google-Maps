4ea04ea4137ddd8050ba790d95d571aa7cbf854d
ricardo moises <solop162@gmail.com>
2023-10-23 19:20:36 -0300

    atualizando

--- api/js/script.js
+++ api/js/script.js
@@ -1,12 +1,15 @@
 let map;
 function initMap() {
 
-    map = new google.maps.Map(document.getElementById('map'), {
-
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
-    }
+    let mapOptions = {
     
-    );
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8,
+            mapTypeId: '' // rodmap, satellite, hybrid, terrain
+    };
+
+    
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
