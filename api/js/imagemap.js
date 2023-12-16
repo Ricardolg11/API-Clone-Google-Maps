28cef1f291119866184a83c9f564b8da7bf51f6a
ricardo moises <solop162@gmail.com>
2023-12-15 22:45:08 -0300

    atualizando

--- api/js/imagemap.js
+++ api/js/imagemap.js
@@ -1,25 +1,32 @@
-let map;
+var map;
 
 function initMap() {
 
-    let mapOptions = {
+    var mapOptions = {
     
-            center: {lat: 3.716816, lng: 38.519115},
-            zoom: 8
+            center: {lat: -3.716816, lng: -38.519115},
+            zoom: 8,
+            mapTypeControlOptions: {
+                mapTypeIds: ['osm']
+            }
     };
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     
     //ImageMapType
 
-    let imageMapType = new google.maps.ImageMapType({
+    var imageMapType = new google.maps.ImageMapType({
         tileSize: new google.maps.Size(256,256),
         getTileUrl: function(coord, zoom) {
-            return '';
+            return 'https://tile.openstreetmap.org/'+ zoom  + ' /' + coord.x + '/'+ coord.y + '.png';
 
         },
         maxZoom: 18
 
     });
 
+    map.mapTypes.set('osm', imageMapType); // setando opensteatmap osm
+    map.setMapType('osm')
+
 }
+
